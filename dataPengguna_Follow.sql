3b9334b820712786d617b6d1a6a31a364f701134
Ketherinegnwn <ketherinegoenawan@gmail.com>
2023-06-08 01:13:42 +0700

    udah 4 tabel

--- dataPengguna_Follow.sql
+++ dataPengguna_Follow.sql
@@ -6,18 +6,17 @@
 --
 -- Table structure for table `pengguna`
 --
-
-DROP TABLE pengguna;
+ DROP TABLE pengguna;
+ DROP TABLE follow;
+-- DROP TABLE review;
+-- DROP TABLE write_review;
 
 CREATE TABLE pengguna
-( nama_lengkap VARCHAR(50) NOT NULL,
-email VARCHAR(50) NOT NULL,
-username VARCHAR(15) NOT NULL,
-password VARCHAR(20) NOT NULL
-);
-
-CREATE INDEX pengguna
-ON pengguna (username);
+( 	nama_lengkap VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	username VARCHAR(15) NOT NULL PRIMARY KEY,
+	password VARCHAR(20) NOT NULL
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `usergroups`
@@ -38,12 +37,14 @@ INSERT INTO pengguna (username,password,nama_lengkap,email) VALUES('Joan', '1234
 --
 
 CREATE TABLE follow
-(	username1 VARCHAR(15) NOT NULL ,
+(	username1 VARCHAR(15) NOT NULL,
 	username2 VARCHAR(15) NOT NULL, 
 	FOREIGN KEY (username1) 
-    REFERENCES pengguna(username),
+    REFERENCES pengguna(username)
+	ON DELETE CASCADE,
 	FOREIGN KEY (username2) 
     REFERENCES pengguna(username)
+	ON DELETE CASCADE
 );
 
 --
@@ -52,3 +53,32 @@ CREATE TABLE follow
 
 INSERT INTO follow (username1,username2) VALUES('Angel' , 'Joan');
 INSERT INTO follow (username1,username2) VALUES('Joan' , 'Angel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE review
+(	review_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	INDEX rev_id(review_id),
+	angka_review INTEGER NOT NULL,
+	teks_review varchar(300),
+	tas_id INTEGER NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `write_review`
+--
+
+CREATE TABLE write_review
+(	tanggal datetime NOT NULL,
+	username VARCHAR(15) NOT NULL,
+	review_id INTEGER NOT NULL,
+	FOREIGN KEY (review_id)
+		references review(review_id)
+		ON DELETE CASCADE
+);
