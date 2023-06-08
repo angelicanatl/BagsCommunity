eab221d44809367aa38c17c984051ffaff65f6c2
Joan Arc <joanedna11@gmail.com>
2023-06-08 13:31:28 +0700

    indeks

--- index.js
+++ index.js
@@ -18,19 +18,22 @@ app.listen(PORT, (err) => {
         console.log(`Server is ready, listening on port ${PORT}`);
     }
 });
+
 const conn = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'bags_community',
     host: 'localhost'
 });
+
 const SessionStore = memoryStore(session);
+
 app.use(session({
     store: new SessionStore({
         checkPeriod: 1*60*60*1000
     }),
     name: 'nama',
-    secret: 'rahasia',
+    secret: 'rahasiasecret',
     resave: false,
     saveUninitialized: false
 }));
@@ -49,14 +52,89 @@ const addPengguna = (conn, data) => {
 app.get('/', (req, res) => {
     res.render('login');
 });
+
 app.get('/signup', (req, res) => {
     res.render('signup');
 });
+
 app.post('/signup', (req, res) => {
     const data = req.body;
     addPengguna(conn,data)
     .then((result) => {
         res.render('login');
     })
+});
+    
+let username = 'joan_nat';
+let nama = 'Joan Natalie'; // nnti ambil dari database
+app.get('/Dasboard', (req, res) => {
+    res.render('Dashboard', {
+        username: username,
+        nama: nama
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        username: username
+    });
+});
+
+app.get('/AdminProfile', (req, res) => {
+    res.render('AdminProfile', {
+        username: username,
+        nama: nama
+    });
+});
+
+const kategori = ['Back Pack', 'Koper', 'Hand Bag', 'Another Bag']
+app.get('/addCatAndSub', (req, res) => {
+    res.render('addCategory', {
+        username: username,
+        kategori: kategori
+    });
+});
+
+const namaKat = 'BackPack';
+// nanti ambil nama kategori yang dipilih buat WHERE sintaks
+const subkategori = ['School backpack', 'Cross Body backpack', 'Mini backpack', 'Laptop backpack'];
+// nanti ambil subkategori di database
+app.get('/addSubCategory', (req, res) => {
+    res.render('addSubCategory', {
+        username: username,
+        kategori: namaKat,
+        subkategori: subkategori
+    });
+});
+
+app.get('/addBagItem', (req, res) => {
+    res.render('addBagItem', {
+        username: username
+    });
+});
+
+const id = 1; // nanti ambil id dari database
+app.get('/uploadFile', (req, res) => {
+    res.render('uploadFile', {
+        id: id
+    });
+});
+
+app.get('/uploadManual', (req, res) => {
+    res.render('uploadManual', {
+        id: id
+    });
+});
+
+app.get('/reviewSettings', (req, res) => {
+    res.render('reviewSettings', {
+        username: username
+    });
+});
+
+app.get('/statistikTas', (req, res) => {
+    res.render('statistikTas', {
+        username: username
+    });
 });
  
